c93dc21895a78bd0a17c449e7706d494754bdd32
Laurent Prevost <laurent.prevost@lotaris.com>
2015-07-01 17:38:06 +0200

    Improved DB constraints

--- db/migrations/20150701162348_improve_constraints.js
+++ db/migrations/20150701162348_improve_constraints.js
@@ -2,19 +2,13 @@
 
 exports.up = function(knex, Promise) {
 	return Promise.resolve()
-		.then(function() { return knex.schema.table('event_types', function (table) { return table.boolean('public'); }); })
-		.then(function() { return knex.raw('UPDATE event_types SET public = true'); })
-		.then(function() { return knex.raw('ALTER TABLE event_types ALTER COLUMN public SET NOT NULL'); })
+		.then(function() { return knex.raw('ALTER TABLE rules ALTER COLUMN name SET NOT NULL'); })
 
-		.then(function() { return knex.schema.table('action_types', function (table) { return table.boolean('public'); }); })
-		.then(function() { return knex.raw('UPDATE action_types SET public = true'); })
-		.then(function() { return knex.raw('ALTER TABLE action_types ALTER COLUMN public SET NOT NULL'); })
 	;
 };
 
 exports.down = function(knex, Promise) {
 	return Promise.resolve()
-		.then(function() { return knex.schema.table('event_types', function(table) { return table.dropColumn('public'); }); })
-		.then(function() { return knex.schema.table('action_types', function(table) { return table.dropColumn('public'); }); })
+		.then(function() { return knex.raw('ALTER TABLE rules ALTER COLUMN name SET NULL'); })
 	;
 };
