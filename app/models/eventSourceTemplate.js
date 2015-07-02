93e8d233e84311c0cf5fc31ba751255f1fdd1c6e
Laurent Prevost <laurent.prevost@lotaris.com>
2015-07-02 13:27:52 +0200

    Added constraint name check for eventSourceTemplate

--- app/models/eventSourceTemplate.js
+++ app/models/eventSourceTemplate.js
@@ -8,7 +8,7 @@ var EventSourceTemplate = module.exports = bookshelf.Model.extend({
 	hasTimestamps: true,
 
 	validations: {
-		name: [ 'required', 'minLength:3', 'unique:event_source_templates:name:Name is already taken.' ]
+		name: [ 'required', 'minLength:3', 'unique:event_source_templates:[name, organization_id]:Name is already taken in this organization.' ]
 	},
 
 	organization: function() {
